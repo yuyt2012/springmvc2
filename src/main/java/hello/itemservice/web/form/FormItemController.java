e542eb69f7264d0202433307b1e84c6d75016779
yuyt2012 <yuyt2012@gmail.com>
2023-08-28 22:34:23 +0900

    0828 22:34

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -11,7 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -21,6 +24,30 @@ public class FormItemController {
 
     private final ItemRepository itemRepository;
 
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        return regions;
+     }
+
+     @ModelAttribute("itemTypes")
+     public ItemType[] itemTypes() {
+         return ItemType.values();
+     }
+
+     @ModelAttribute("deliveryCodes")
+     public List<DeliveryCode> deliveryCodes() {
+         List<DeliveryCode> deliveryCodes = new ArrayList<>();
+         deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+         deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+         deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+
+         return deliveryCodes;
+     }
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
@@ -32,12 +59,14 @@ public class FormItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
         return "form/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
+
         return "form/addForm";
     }
 
@@ -45,6 +74,7 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
 
         log.info("item.open={}",item.getOpen());
+        log.info("item.regions={}", item.getRegions());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -56,6 +86,7 @@ public class FormItemController {
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
         return "form/editForm";
     }
 
